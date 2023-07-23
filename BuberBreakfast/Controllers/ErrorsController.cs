4ea2027ea87ff9e2dbff661a6e8b4bf4a10818cf
SaiyedRushan <rushan52@gmail.com>
2023-07-23 13:05:42 -0400

    added error handling

---
+++ BuberBreakfast/Controllers/ErrorsController.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Mvc;
+namespace BuberBreakfast.Controllers;
+
+public class ErrorsController : ControllerBase
+{
+    [Route("/error")]
+    public IActionResult Error() => Problem();
+}
